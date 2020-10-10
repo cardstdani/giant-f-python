0bfdd217e1ede782a95546df8efec9f3b84e569c
cardstdani <72665778+cardstdani@users.noreply.github.com>
2020-10-10 20:03:46 +0200

    Create MainClass.py

---
+++ MainClass.py
@@ -0,0 +1,6 @@
+sizeMultiplier = 1
+for a in range(35 * sizeMultiplier):
+      if((a * sizeMultiplier) < ((5 * sizeMultiplier) * sizeMultiplier) or ((a * sizeMultiplier) > ((8 * sizeMultiplier) * sizeMultiplier) and (a * sizeMultiplier) < ((14 * sizeMultiplier) * sizeMultiplier))):
+        print("F" * 35 * sizeMultiplier)
+      else:
+        print("F" * 10 * sizeMultiplier)
