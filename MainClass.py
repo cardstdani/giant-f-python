46d56e55c848ab1ec6ab9a2603afda8d389ae755
cardstdani <72665778+cardstdani@users.noreply.github.com>
2020-10-10 20:07:21 +0200

    Update MainClass.py

--- MainClass.py
+++ MainClass.py
@@ -3,4 +3,4 @@ for a in range(35 * sizeMultiplier):
       if((a * sizeMultiplier) < ((5 * sizeMultiplier) * sizeMultiplier) or ((a * sizeMultiplier) > ((8 * sizeMultiplier) * sizeMultiplier) and (a * sizeMultiplier) < ((14 * sizeMultiplier) * sizeMultiplier))):
         print("F" * 35 * sizeMultiplier)
       else:
-        print("F" * 10 * sizeMultiplier)
+        print("F" * 12 * sizeMultiplier)
